f1f91f2d4432e8d09c25947f6f636245ab0a6df1
Marlon Frazão <frazao.marlon@gmail.com>
2025-12-30 12:59:44 -0300

    initial commit

---
+++ src/main/java/com/marlon/config/GreetingConfiguration.java
@@ -0,0 +1,6 @@
+package com.marlon.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("greeting-service")
+public record GreetingConfiguration(String greeting, String defaultValue) {}
